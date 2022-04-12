bec444a6663f3e3b4caa1bbcfb7ad62640cca240
Mojatto <daichiandroid1997+work@gmail.com>
2022-04-13 01:30:17 +0900

    CHANGE:Build.cs の記述に誤りがあったため修正．

--- Source/MJT_FireworksGame/MJT_FireworksGame.Build.cs
+++ Source/MJT_FireworksGame/MJT_FireworksGame.Build.cs
@@ -1,5 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class MJT_FireworksGame : ModuleRules
@@ -19,58 +21,61 @@ public class MJT_FireworksGame : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+		LoadDiscordSDK(Target);
 	}
-}
 
-private string ModulePath
-{
-	get { return ModuleDirectory; }
-}
+	private string ModulePath
+	{
+		get { return ModuleDirectory; }
+	}
 
-private string DiscordPath
-{
-	get { return Path.GetFullPath(Path.Combine(ModulePath, "..", "Discord")); }
-}
+	private string DiscordPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModulePath, "..", "Discord")); }
+	}
 
-private string DiscordSDKDLLName
-{
-	get { return "discord_game_sdk"; }
-}
+	private string DiscordSDKDLLName
+	{
+		get { return "discord_game_sdk"; }
+	}
 
-private void AddDiscordLibPathWindows(ReadOnlyTargetRules Target)
-{
-	string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x86_64" : "x86";
-	
-	string dll = Path.Combine(DiscordPath, lib, PlatformString, DiscordSDKDLLName + ".dll");
-	
-	PublicAdditionalLibraries.Add(dll + ".lib"));
-	RuntimeDependencies.Add(dll));
-}
+	private void AddDiscordLibPathWindows(ReadOnlyTargetRules Target)
+	{
+		// string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x86_64" : "x86";
+		string PlatformString = "x86_64";
+		
+		string dll = Path.Combine(DiscordPath, "lib", PlatformString, DiscordSDKDLLName + ".dll");
+		
+		PublicAdditionalLibraries.Add(dll + ".lib");
+		RuntimeDependencies.Add(dll);
+	}
 
-private void AddDiscordLibPathMac(ReadOnlyTargetRules Target)
-{
-	string dll = Path.Combine(DiscordPath, lib, "Mac", DiscordSDKDLLName + ".bundle");
-	RuntimeDependencies.Add(dll));
-}
+	private void AddDiscordLibPathMac(ReadOnlyTargetRules Target)
+	{
+		string dll = Path.Combine(DiscordPath, "lib", "Mac", DiscordSDKDLLName + ".bundle");
+		RuntimeDependencies.Add(dll);
+	}
 
-public bool LoadDiscordSDK(ReadOnlyTargetRules Target)
-{
-    if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetnPlatform.Win32))
-    {
-		PublicDefinitions.Add("_EG_WINDOWS_PLATFORM");
-		AddDiscordLibPathWindows(Target);
-    }
-	else if (Target.Platform == UnrealTargetPlatform.Mac)
-    {
-		PublicDefinitions.Add("_EG_IMAC_PLATFORM");
-		AddDiscordLibPathMac(Target);
-    }
-	else
-    {
-		throw new Exception("\nTarget platform not supported: " + Target.Platform);
-    }
+	public bool LoadDiscordSDK(ReadOnlyTargetRules Target)
+	{
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicDefinitions.Add("_EG_WINDOWS_PLATFORM");
+			AddDiscordLibPathWindows(Target);
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicDefinitions.Add("_EG_IMAC_PLATFORM");
+			AddDiscordLibPathMac(Target);
+		}
+		else
+		{
+			throw new Exception("\nTarget platform not supported: " + Target.Platform);
+		}
 
-	PublicIncludePaths.Add(Paths.Combine(DiscordPath, "includes"));
+		PublicIncludePaths.Add(Path.Combine(DiscordPath, "includes"));
 
-	return true;
+		return true;
+	}
 }
